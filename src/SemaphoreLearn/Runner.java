18502300a3ac407aea33cb7842d9ed1320692033
rli1994hi <rli1994hi@gmail.com>
2022-06-11 09:11:06 -0700

    semaphore. simulate race condition for partial acquire and extra release

--- src/SemaphoreLearn/Runner.java
+++ src/SemaphoreLearn/Runner.java
@@ -6,13 +6,17 @@ public class Runner {
     public static void main(String[] args) throws InterruptedException {
         Semaphore semaphore = new Semaphore(1);
         Thread t1 = new Thread(new SemaphoreGrabberRunnable(semaphore, 2)); // 2 is greater than available permits 1
-        Thread t2 = new Thread(new SemaphoreCheckerRunnable(semaphore));
+        Thread t2 = new Thread(new SemaphoreGrabberAndReleaserRunnable(semaphore, 1, 2));
+        Thread t3 = new Thread(new SemaphoreCheckerRunnable(semaphore));
 
         t1.start();
         Thread.sleep(1000);
         t2.start();
+        Thread.sleep(1000);
+        t3.start();
 
         t1.join();
         t2.join();
+        t3.join();
     }
 }
--- src/SemaphoreLearn/SemaphoreGrabberAndReleaserRunnable.java
+++ src/SemaphoreLearn/SemaphoreGrabberAndReleaserRunnable.java
@@ -2,16 +2,17 @@ package SemaphoreLearn;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Date;
 import java.util.concurrent.Semaphore;
 
-public class SemaphoreGrabberRunnable implements Runnable {
+public class SemaphoreGrabberAndReleaserRunnable implements Runnable {
     private Semaphore semaphore;
     private int permitsToGrab;
+    private int permitsToRelease;
 
-    public SemaphoreGrabberRunnable(Semaphore semaphore, int permitsToGrab) {
+    public SemaphoreGrabberAndReleaserRunnable(Semaphore semaphore, int permitsToGrab, int permitsToRelease) {
         this.semaphore = semaphore;
         this.permitsToGrab = permitsToGrab;
+        this.permitsToRelease = permitsToRelease;
     }
 
     @Override
@@ -21,14 +22,25 @@ public class SemaphoreGrabberRunnable implements Runnable {
                     LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS")),
                     Thread.currentThread().getName(),
                     permitsToGrab); // TODO: set up log format
-
             semaphore.acquire(permitsToGrab);
             System.out.printf("[%s] [%s] Grabbed semaphore for %d permits%n",
                     LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS")),
                     Thread.currentThread().getName(),
                     permitsToGrab);
+
+            System.out.printf("[%s] [%s] Releasing semaphore for %d permits%n",
+                    LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS")),
+                    Thread.currentThread().getName(),
+                    permitsToRelease);
+            semaphore.release(permitsToRelease);
+            System.out.printf("[%s] [%s] Released semaphore for %d permits%n",
+                    LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS")),
+                    Thread.currentThread().getName(),
+                    permitsToRelease);
+
         } catch (Exception e) {
 
         }
     }
+
 }
